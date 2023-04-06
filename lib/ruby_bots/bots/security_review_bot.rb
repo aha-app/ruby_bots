d915d5928d7e33d2a5e4788d73c777ec5ce3ffd6
Andrew Jones <andrewjones232@gmail.com>
2023-04-06 11:30:03 -0700

    improved ruby prompt

--- lib/ruby_bots/bots/security_review_bot.rb
+++ lib/ruby_bots/bots/security_review_bot.rb
@@ -10,25 +10,39 @@ module RubyBots
       <<~'PROMPT'
       You are assisting a code review for a multi-tenant Ruby on Rails application. Your primary focus is identifying potential security vulnerabilities. 
 
-      You will be provided Ruby code and you should flag any lines that contain a potential vulnerability. You should consider every line, but most lines will not have a vulnerability and for those you should say the line number in Lx format followed by "OK". Example for a line four where you have no concerns: "L4 OK". Otherwise, you should say the line number followed by "unsafe:" followed by the code itself.  
-
-      Here are some specific kinds of vulnerabilities preceded by "unsafe:" with a comment explaining the concern, with a safe "suggestion:" if applicable. Keep in mind that you are not limited to these specific examples when detecting vulnerabilities.
-
+      You will be provided ruby code and you should flag any lines that contain a potential vulnerability or concern. You should consider every line, but most lines will not have an issue and for those you should say the line number in Lx format followed by "OK". Example for a line four where you have no concerns: "L4 OK". Otherwise, you should say the line number followed by "unsafe:" followed by the code itself.  
+      
+      Here are some specific kinds of vulnerabilities preceded by "unsafe:" with a comment explaining the concern, with a safe "suggestion:" if applicable. The suggestion may either be ruby code or a higher level consideration in plain text. Keep in mind that you are not limited to these specific examples when detecting vulnerabilities.
+      
       It is unsafe to run queries directly on ActiveRecord models, which will look like a capitalized word followed by .find or .where, like "Test.find(params[:id))". Instead they should be queried associated with Account.current to prevent cross-account data access. Example: 
       L1 unsafe: Feature.find(params[:id]) 
-      suggestion: Account.current.features.find(params[:id])
-
+      suggestion: ```ruby
+      Account.current.features.find(params[:id])
+      ```
+      
       It is unsafe to directly interpolate strings into SQL queries because of SQL injection. Many ActiveRecord methods like exists? and order can also be unsafe when used with params. Example:
       L1 unsafe: Account.current.features.where("features.workflow_status_id IN #{params[:workflow_status_ids]}") # vulnerable to sql injection
-      suggestion: Account.current.features.where("features.workflow_status_id IN (?)", workflow_status_ids) # bind params are safe
-
+      suggestion: ```ruby
+      Account.current.features.where("features.workflow_status_id IN (?)", workflow_status_ids) # bind params are safe
+      ```
+      
       L1 OK
       L2 OK
-      L3 unsafe: user_input.constantize 
-
+      L3 unsafe: user_input.constantize
+      suggestion: Use a whitelist to ensure only expected class values can used for constantize.
+      
       L1 unsafe: User.exists?(params[:user_id]) # vulnerable to SQL injection
-
+      
       L1 unsafe: redirect_to params[:return_to] # vulnerable to open redirect
+      suggestion: ensure that the return_to value is safe for redirection.
+      
+      L1 OK
+      L2 OK
+      L3 OK
+      L4 unsafe: open(params[:filename]) { |file| file.read } # unsafe because Kernel#open executes OS command if the argument starts with a vertical bar (|).
+      suggestion: Use a safer method for opening the file. ```ruby
+      File.open(params[:filename]) { |file| file.read } if allowed_filename?(params[:filename]) # File.open doesn't execute commands
+      ```      
       PROMPT
     end
 
